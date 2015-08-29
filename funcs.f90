2ab12cd2ec87e2cde712210e2d9cb57a4cdad103
wtffqbpl <renyj@buaa.edu.cn>
2015-08-29 23:44:22 +0800

    final version

--- funcs.f90
+++ funcs.f90
@@ -5,8 +5,23 @@ module globalvar
 ! Function: define some global variables
 !
 !**************************************************************************
-    real,allocatable    :: lambda(:),vecij(:,:),Uobs(:),matA(:,:),matb(:)
-    integer             :: Nlocations
+! 参数说明：
+!   lambda         --- kriging coefficients
+!	vecij			--- 样本坐标(维数：[3,N])
+!	Uobs			---	U^obs(N)
+!	matA			--- eqn.(2) 中矩阵A的系数, [N,N]
+!	matb			---	eqn.(2) 中右端项b [N]
+!	rhserr			--- RHS error [N]
+!	diatancesA		---	Matrix of distances
+!	Nlocations		---	样本总点数
+!	numsample		---	计算中需要的样本点数
+!	numreqloc		---	计算中需要估计的点数
+!	miuU			---	样本U^obs的平均值
+!	sill,nugget,range	---	eqn.(6)中的几个参数
+
+	real,allocatable    :: lambda(:),vecij(:,:),Uobs(:),matA(:,:), &
+						   matb(:),rhserr(:),distancesA(:,:)
+	integer             :: Nlocations 
     real                :: numsample,numreqloc
     real                :: miuU,sill,nugget,range
 
@@ -19,12 +34,12 @@ subroutine readin
 !**************************************************************************
 !
 ! Function: read samples values in the file "Cooper[Samples].txt 
-!           and allocate variables
+!           and allocate memories for variables
 !
 !**************************************************************************
 !
 ! Local variables
-    integer     :: inunit
+    integer     :: inunit,i
     character   :: inputfile*30
     logical     :: succeed
 
@@ -33,7 +48,9 @@ subroutine readin
 ! Read control file and some variables
     inputfile = 'control.txt'
     inquire(file=inputfile,exist=succeed)
-    if(.ne. succeed) then
+    if(.not. succeed) then
+		write(*,*)"没有控制文件control.txt"
+		call errmsg(2)
 ! TODO  write error msg in diagnoistics file
         stop
     end if
@@ -46,9 +63,11 @@ subroutine readin
     close(inunit)
 
 !   Read sample values in the file "Cooper[Samples].txt"
-    inputfile = 'Cooper[Samples].txt'
-    inquire(file=inputfile,exist=succeed)
-    if(.ne. succeed) then
+    inputfile = 'Copper[Samples].txt'
+    inquire(file=trim(inputfile),exist=succeed)
+    if(.not. succeed) then
+		write(*,*)"没有Copper[Samples].txt文件"
+		call errmsg(3)
 ! TODO  write error msg in diagnoistics file
         stop
     end if
@@ -56,13 +75,20 @@ subroutine readin
     read(inunit,*)
     read(inunit,*) Nlocations
     if(numsample .gt. Nlocations) then 
-    !TODO write error msg in the diagnostics file
+		write(*,*)"需要的样本数大于目前所能提供的样本数"
+		write(*,*)"目前的样本共有：",Nlocations
+		write(*,*)"控制文件需要的样本数有:",numsample
+		call errmsg(4)
         stop
     end if
     Nlocations = numsample
 
-    allocate(vecij(3,Nlocations),Uobs(Nlocations),matA(Nlocations,Nlocations),
-             matb(Nlocations),lambda(Nlocations))
+	! write Nlocations in diagnostics file
+	call diagnostics(2,0)
+
+    allocate(vecij(3,Nlocations),Uobs(Nlocations),matA(Nlocations,Nlocations), &
+             matb(Nlocations),lambda(Nlocations),rhserr(Nlocations), &
+			 distancesA(Nlocations,Nlocations))
     read(inunit,*)
     do i=1,Nlocations
         read(inunit,*)vecij(1,i),vecij(2,i),vecij(3,i),Uobs(i)
@@ -150,12 +176,59 @@ subroutine Upred_comp(Upred)
 end subroutine Upred_comp 
 
 
+
 subroutine LUDecomposition
     use globalvar
     implicit none 
 !**************************************************************************
 !
-! Function: Solve linear equation A * Lambda = b with LU decomposition
+! Function:  LU decomposition matrix A (A=LU)
+!         matrix L and U are stored in A
+!
+!**************************************************************************
+!
+! Local variables
+	integer	:: r,i,k
+	
+
+	! 分解A=LU，L和U存放在matA中
+	do r=1,Nlocations
+		!计算U的第r行
+		do i=r,Nlocations
+			do k=1,r-1
+				matA(r,i)=matA(r,i)-matA(r,k)*matA(k,i)
+			end do
+		end do
+
+		!计算L的第r列
+		do i=r+1,Nlocations
+			do k=1,r-1
+				matA(i,r)=matA(i,r)-matA(i,k)*matA(k,r)
+			end do
+			matA(i,r)=matA(i,r)/matA(r,r)
+		end do
+	end do
+
+	return
+end subroutine LUDecomposition
+
+
+
+
+
+
+
+
+
+
+
+
+subroutine solvelineareq
+    use globalvar
+    implicit none 
+!**************************************************************************
+!
+! Function: Solve linear equation LU * Lambda = b
 !           method ( eqn. (2) )
 !
 !**************************************************************************
@@ -163,9 +236,26 @@ subroutine LUDecomposition
 ! Local variables
 
 ! TODO compute Lambda
+	integer	:: i,k
+	
+	!求解Ly=b，y放入x中
+	do i=1,Nlocations
+		lambda(i)=matb(i)
+		do k=1,i-1
+			lambda(i)=lambda(i)-matA(i,k)*lambda(k)
+		end do
+	end do
+	!求解Ux=y
+	do i=Nlocations,1,-1
+		do k=Nlocations,i+1,-1
+			lambda(i)=lambda(i)-matA(i,k)*lambda(k)
+		end do
+		lambda(i)=lambda(i)/matA(i,i)
+	end do
 
 
-end subroutine LUDecomposition
+	return
+end subroutine solvelineareq
 
 
 subroutine coeffformatxA
@@ -179,11 +269,12 @@ subroutine coeffformatxA
 !
 ! Local variables
     integer :: i, j
-    real    :: distances,gammal
+    real    :: distances,gammal,covaij
 
 !  Compute coefficients of Matrix A with eqn. (3)
     do i=1,Nlocations
         do j=1,Nlocations
+			call distancefunc(vecij(:,j),vecij(:,i),distancesA(j,i))
             call CovarianceComp(vecij(:,j),vecij(:,i),nugget,sill,range,covaij)
             matA(j,i) = covaij
         end do
@@ -207,7 +298,7 @@ subroutine coeffformatxb(vec_zero)
 
 ! Local variables
     integer :: i, j
-    real    :: distances,gammal
+    real    :: distances,gammal,covaij
 
 !  Compute coefficients of Vector b with eqn. (4)
     do i=1,Nlocations
@@ -253,7 +344,7 @@ subroutine gammalfunc(l,nugget,sill,range,gammaL)
 !*************************************************************************
 
 ! Arguments:
-    real    :: l,nugget,sill,range,gammaL,
+    real    :: l,nugget,sill,range,gammaL
 
 ! Local variables
     
@@ -264,3 +355,127 @@ subroutine gammalfunc(l,nugget,sill,range,gammaL)
     end if
     return 
 end subroutine gammalfunc
+
+
+subroutine rhserrcomp
+	use globalvar
+	implicit none 
+!*************************************************************************
+!
+! Function: compute RHS error (RHSERR = b - A * Lambda)
+!
+!*************************************************************************
+!
+! Local variables
+	integer :: i,j
+
+	rhserr = 0.0
+
+	do i=1,Nlocations
+		do j=1,Nlocations
+			rhserr(i) = rhserr(i) + matA(i,j)*lambda(j)
+		end do
+		rhserr(i) = matb(i)-rhserr(i)
+	end do
+
+	call diagnostics(9,0)
+
+	return 
+end subroutine rhserrcomp
+
+
+
+subroutine errmsg(err_type)
+	implicit none 
+!*************************************************************************
+!
+! Function: print error msg
+!
+!*************************************************************************
+!
+! Arguments:
+	integer :: err_type
+! Local variables
+	character :: errmsgfile*30
+	integer	  :: errunit
+
+	errmsgfile='Cooper[errors].txt'
+	errunit = 2200
+
+	open(errunit,file=errmsgfile)
+	select case(err_type)
+		case(1)
+			write(errunit,*)"不存在Cooper[reqloc].txt文件"
+		case(2)
+			write(errunit,*)"没有控制文件control.txt"
+		case(3)
+			write(errunit,*)"没有Copper[Samples].txt文件"
+		case(4)
+			write(errunit,*)"需要的样本数大于目前所能提供的样本数"
+	end select
+
+	return 
+end subroutine errmsg
+
+
+subroutine diagnostics(msg_iflag,ireq)
+	use globalvar
+	implicit none 
+!*************************************************************************
+!
+! Function: write intermiediate calculations to a "diagnostics file"
+!
+!*************************************************************************
+!
+! Arguments:
+	integer :: msg_iflag,ireq
+! Local variables
+	integer	:: diagunit,i,j
+	character :: diagfile*30
+	
+	diagunit=1000
+	diagfile='Copper[Diagnostics].txt'
+
+	select case(msg_iflag)
+		case(0)
+			open(diagunit,file=diagfile)
+		case(1)
+			!open(diagunit,file=diagfile,
+			write(diagunit,*)'Diagnostic file'
+		case(2)
+			write(diagunit,*)Nlocations
+		case(3)
+			write(diagunit,*)miuU
+		case(4)
+			write(diagunit,*)
+			write(diagunit,*)"Matrix of distances"
+		case(5)
+			write(diagunit,*)
+			write(diagunit,*)"Covariance matrix A"
+			do i=1,Nlocations
+				write(diagunit,'(100(E14.4))')(matA(i,j),j=1,Nlocations)
+			end do
+		case(10)
+			write(diagunit,*)
+			write(diagunit,*)"Matrix of distances"
+			do i=1,Nlocations
+				write(diagunit,'(100(E14.4))')(distancesA(i,j),j=1,Nlocations)
+			end do
+		case(6)
+			write(diagunit,*)numreqloc
+		case(7)
+			write(diagunit,*)
+			write(diagunit,*)ireq
+			write(diagunit,*)'Covariance vector b'
+			write(diagunit,'(100(E14.4))')(matb(i),i=1,Nlocations)
+		case(8)
+			write(diagunit,*)'Kriging coefficients Lambda'
+			write(diagunit,'(100(E14.4))')(lambda(i),i=1,Nlocations)
+		case(9)
+			write(diagunit,*)'RHS error'
+			write(diagunit,'(100(E14.4))')(rhserr(i),i=1,Nlocations)
+	end select
+
+	return 
+end subroutine diagnostics
+
--- main.f90
+++ main.f90
@@ -8,16 +8,22 @@ program main
 !**************************************************************************
 !
 ! Local variables
-    integer     :: requnit,resunit
-    character   :: reqlocfile*30,resultsfile*30
-    real        :: vecreqloc(3)
-    integer :: nreqLocs
+    integer     :: requnit,resunit,exunit
+    character   :: reqlocfile*30,resultsfile*30,excelfile*30
+    real        :: vecreqloc(3),UpredReqLoc
+    integer     :: nreqLocs,i
     logical     :: succeed
 
     requnit = 1300
     resunit = 1400
-    reqlocfile  = 'Cooper[ReqLoc].txt'
-    resultsfile = 'Cooper[results].txt'
+	exunit  = 1500
+    reqlocfile  = 'Copper[ReqLoc].txt'
+    resultsfile = 'Copper[results].txt'
+	excelfile	= 'results_excel.csv'
+
+! open diagnostic file and write simple title
+	call diagnostics(0,0)
+	call diagnostics(1,0)
 
 !   Read dataset of measurements {U1_obs, u2_obs, ..., Un_obs} and 
 !   corresponding locations {v1,v2,...,vn}
@@ -25,19 +31,31 @@ program main
     
 !   Calculating the average Miu_u of a set of U values
     call AverageMiuU
+!   Write anverage Miu_u in diagnostics file
+	call diagnostics(3,0)
 
 !   Calculating the coefficients of matrix A
     call coeffformatxA
+!	write distances and Covariance matrix A in diagnostics file
+	call diagnostics(10,0)
+	call diagnostics(5,0)
 
-!   open results file
+!	LU decomposition matrix A and store matrix L and U in A
+	call  LUDecomposition
+
+!   open results file and results for excel
     open(resunit,file=resultsfile)
+	open(exunit,file=excelfile)
 
-!   check "Cooper[ReqLoc].txt"
+!   check "Copper[ReqLoc].txt"
     inquire(file=reqlocfile,exist=succeed)
-    if(.ne. succeed) then
+    if(.not. succeed) then
+		write(*,*)"不存在Cooper[reqloc].txt文件"
+		call errmsg(1)
     ! TODO print error msg in diagnostics file
+		stop
     end if
-!   Cooper[ReqLoc].txt exists, then open and read it
+!   Copper[ReqLoc].txt exists, then open and read it
     open(requnit,file=reqlocfile)
     read(requnit,*)
     read(requnit,*) nreqLocs
@@ -47,25 +65,43 @@ program main
     end if
     nreqLocs = numreqloc
 
-!   write nReq in "Cooper[results].txt"
+!   write nReq in "Copper[results].txt" and excel output file
+	write(exunit,*)"Num,X,Y,Z,U^pred"
     write(resunit,*) nreqLocs
+!	write nReq in diagnostics file
+	call diagnostics(6,0)
+
     read(requnit,*)
     do i=1,nreqLocs
         read(requnit,*) vecreqloc(1),vecreqloc(2),vecreqloc(3)
+
+!      ex(1.9) 添加Local kriging算法的地方
+!		在这里添加子程序，在样本中寻找与所需坐标最相近的坐标点K，
+!		利用K点来进行如下的操作即可完成Local kriging算法
     ! compute coefficients of matrix b 
         call coeffformatxb(vecreqloc)
+	! write covariance vector b in diagnostics file
+		call diagnostics(7,i)
     ! LU decomposition method for solving A * Lambda = b 
-        call LUDecomposition
+        call solvelineareq
+	! write Kriging coefficients Lambda in diagnostics file
+		call diagnostics(8,0)
     ! Compute U^pred at location VecReqLoc
         call Upred_comp(UpredReqLoc)
-    ! Write ReqLocs and kriging estimator U^pred in Cooper[results].txt
+    ! Write ReqLocs and kriging estimator U^pred in Copper[results].txt
         write(resunit,1401) i,vecreqloc(1),vecreqloc(2),vecreqloc(3),UpredReqLoc
+		write(exunit, 1501) i,vecreqloc(1),vecreqloc(2),vecreqloc(3),UpredReqLoc
+
+	! Compute RHS error and ouput in Copper[diagnostics].txt
+		call rhserrcomp
     end do
 
     close(resunit)
     close(requnit)
+	close(exunit)
 
 !**************************************************************************
 1401 format(3x,I3,4(2X,F18.10))
+1501 format(I3,',',4(F18.10,','))
 
 end program main
